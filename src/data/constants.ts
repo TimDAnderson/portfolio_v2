626cf62fbbe2148a4b43c33fe4e94e1f2ebcec3d
Tim Anderson <tdanderson805@gmail.com>
2022-09-13 22:02:04 -0700

    init portfolio

---
+++ src/data/constants.ts
@@ -0,0 +1,25 @@
+const constants = {
+  logo: "Logo.svg",
+  name: "Tim Anderson",
+  title: "Web Developer",
+  techs: [
+    "Javascript!",
+    "Typescript",
+    "React",
+    "Next.JS",
+    "Figma",
+    "HTML",
+    "CSS",
+    "TailwindCSS",
+    "ReactNative",
+    "Electron",
+  ],
+  mail: "me@TimDAnderson.com", // for contact form
+  social_media: {
+    github: "https://github.com/timdanderson",
+    linkedin: "https://www.linkedin.com/in/mastagoon/",
+    messenger: "https://m.me/sahal.omar0",
+  },
+}
+
+export default constants
---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { NextPage } from "next"
+import Head from "next/head"
+import About from "../components/About"
+import ContactMe from "../components/ContactMe"
+import Hero from "../components/Hero"
+import MyWork from "../components/MyWork"
+import NavBar from "../components/NavBar"
+import SocialMediaIcons from "../components/SocialMediaIcons"
+import constants from "../data/constants"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          {constants.name} | {constants.title}
+        </title>
+      </Head>
+      <NavBar />
+      <Hero />
+      <About />
+      <MyWork />
+      <ContactMe />
+      <SocialMediaIcons />
+      <div className="flex text-primary-green flex-col justify-center items-center mt-4">
+        <a
+          target={"_blank"}
+          rel="noreferrer"
+          href="https://github.com/timdanderson"
+        >
+          <FontAwesomeIcon
+            className="text-light-gray cursor-pointer hover:text-white transition-all mb-1"
+            size="3x"
+            icon={faGithub}
+          />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Home
